03b0e681205b287c583f03a81627223bcfc942f4
aswinxd <musiqorobot@gmail.com>
2025-02-11 14:07:51 +0530

    string

---
+++ sender/plugins/start.py
@@ -0,0 +1,21 @@
+import asyncio
+from strings.string import p start_command_instructions
+from pyrogram import Client, filters, enums
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from sender import app
+
+
+@app.on_message(filters.command("start") & filters.private)
+async def handle_start_command(client, message):
+    buttons = [
+        [
+            InlineKeyboardButton("add to group", url="https://t.me/MIssALeenA_BOT?startgroup=true"),
+        ],
+        [
+            InlineKeyboardButton("channel", url="https://t.me/elitesbots")
+        ]
+    ]
+    await message.reply_text(
+        start_command_instructions,
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
---
+++ strings/string.py
@@ -0,0 +1,5 @@
+start_command_instructions = (
+    "Welcome! This is anonymous sender bot. you can use this bot to send message to your group\n"
+    "• use /connect on group to connect the bot to your account (group admins only)/n"
+    "• use /chats on inbox to create messages as you like"
+)
